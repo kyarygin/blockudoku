6d1f4a6cbb84a0a1df35d810ad144aaae37f47b6
Yarygin Konstantin Sergeevich <kyarygin@ozon.ru>
2022-06-06 19:16:19 +0300

    Game Class + immutable board + immutable figure

--- game.py
+++ game.py
@@ -2,8 +2,10 @@ import random
 
 from dataclasses import dataclass, field
 from itertools import product, permutations
-from typing import Tuple, List, Union
+from typing import Tuple, List, Set, Union, Optional
 from copy import deepcopy
+
+from time import time
 
 
 BOARD_SIZE = 9
@@ -11,12 +13,12 @@ BLOCK_SIZE = 3
 
 @dataclass
 class Figure:
-    shape: List[List[int]] = field(default_factory=list)
-    size: List[int] = field(init=False)
+    shape: Tuple[Tuple[int]] = field(default_factory=tuple)
+    size: Tuple[int] = field(init=False)
     mass: int = field(init=False)
 
     def __post_init__(self):
-        self.size = [len(self.shape), len(self.shape[0])]
+        self.size = (len(self.shape), len(self.shape[0]))
         self.mass = sum(sum(row) for row in self.shape)
 
     def __getitem__(self, pos: Tuple[int, int]) -> int:
@@ -24,7 +26,8 @@ class Figure:
         return self.shape[i][j]
 
     def __hash__(self):
-        return hash(tuple(tuple(tile for tile in row) for row in self.shape))
+        # return hash(tuple(tuple(tile for tile in row) for row in self.shape))
+        return hash(self.shape)
 
     def __str__(self) -> str:
         output = '\n'.join(
@@ -50,13 +53,13 @@ class FigureSet:
             raw_data = f.read().strip()
         figures = [
             Figure(
-                shape=[
-                    [
-                        int(tile)
-                        for tile in raw_row.split(' ')
-                    ]
+                shape=tuple(
+                    tuple(
+                        int(raw_tile)
+                        for raw_tile in raw_row.split(' ')
+                    )
                     for raw_row in raw_figure.split('\n')
-                ]
+                )
             )
             for raw_figure in raw_data.split('\n\n')
         ]
@@ -66,9 +69,9 @@ class FigureSet:
         return random.choices(self.storage, k=n)
 
 @dataclass
-class Board:
-    state: List[List[int]] = field(
-        default_factory=lambda: [[0 for j in range(BOARD_SIZE)] for i in range(BOARD_SIZE)]
+class BoardState:
+    state: Tuple[Tuple[int]] = field(
+        default_factory=lambda: tuple(tuple(0 for j in range(BOARD_SIZE)) for i in range(BOARD_SIZE))
     )
 
     def __str__(self) -> str:
@@ -96,64 +99,92 @@ class Board:
                 return False
         return True
 
-    def add_figure(self,
-                   figure: Figure,
-                   i: int, j: int,
-                   inplace=True) -> Union[int, Tuple['Board', int]]:
-        board_to_change = self if inplace else Board(deepcopy(self.state))
-        for fi, fj in product(range(figure.size[0]), range(figure.size[1])):
-            board_to_change[fi+i, fj+j] += figure[fi, fj]
-        score = figure.mass
-        if inplace:
-            return score
-        else:
-            return board_to_change, score
+    def possible_figure_places(self, figure: Figure) -> List[Tuple[int, int]]:
+        possible_places = [
+            (i, j)
+            for (i, j) in product(range(BOARD_SIZE), range(BOARD_SIZE))
+            if self.does_figure_fits(figure, i, j)
+        ]
+        return possible_places
 
-    def update(self) -> int:
-        full_rows = [
+    def add_figure(self, figure: Figure, fi: int, fj: int) -> 'BoardState':
+        new_state = tuple(
+            tuple(
+                self[i, j] + figure[i-fi, j-fj]
+                if (fi <= i < fi+figure.size[0]) and (fj <= j < fj+figure.size[1])
+                else self[i, j]
+                for j in range(BOARD_SIZE)
+            )
+            for i in range(BOARD_SIZE)
+        )
+        return BoardState(new_state)
+
+    def update(self) -> 'BoardState':
+        full_rows = {
             i for i in range(BOARD_SIZE)
             if all(self[i, j] for j in range(BOARD_SIZE))
-        ]
-        full_cols = [
+        }
+        full_cols = {
             j for j in range(BOARD_SIZE)
             if all(self[i, j] for i in range(BOARD_SIZE))
-        ]
-        full_blocks = [
+        }
+        full_blocks = {
             (bi, bj) for (bi, bj) in product(range(BOARD_SIZE // BLOCK_SIZE),
                                              range(BOARD_SIZE // BLOCK_SIZE))
             if all(
                 self[bi*BLOCK_SIZE+i, bj*BLOCK_SIZE+j]
                 for i, j in product(range(BLOCK_SIZE), range(BLOCK_SIZE))
             )
-        ]
-        for i, j in product(full_rows, range(BOARD_SIZE)):
-            self[i, j] = 0
-        for i, j in product(range(BOARD_SIZE), full_cols):
-            self[i, j] = 0
-        for (bi, bj), i, j in product(full_blocks, range(BLOCK_SIZE), range(BLOCK_SIZE)):
-            self[bi*BLOCK_SIZE+i, bj*BLOCK_SIZE+j] = 0
-        multiplier = len(full_rows) + len(full_cols) + len(full_blocks)
-        return multiplier
+        }
+        new_state = tuple(
+            tuple(
+                0
+                if (i in full_rows) or (j in full_cols) or ((i // 3, j // 3) in full_blocks)
+                else self[i, j]
+                for j in range(BOARD_SIZE)
+            )
+            for i in range(BOARD_SIZE)
+        )
+        # multiplier = len(full_rows) + len(full_cols) + len(full_blocks)
+        return BoardState(new_state)
+
 
 class Game(object):
 
     def __init__(self):
         self.figure_set = FigureSet.load('data/figures')
-        self.current_board = Board()
+        self.current_board_state = BoardState()
         self.current_score = 0
         self.current_round = 0
 
         self.current_series_multiplier = 1
 
-    def round(self, seed=0):
-        three_figures = self.figure_set.sample(3)
-        figures_orders = {p for p in permutations(three_figures)}
-        output_boards = []
+    def get_new_figures(self, n: int=3, seed: int=0) -> List[Figure]:
+        random.seed(seed)
+        return self.figure_set.sample(n)
 
-        # for order in figures_orders:
-        #     for figure in order:
-        #         for i, j in product(range(BOARD_SIZE), range(BOARD_SIZE)):
-        #             if
+    def possible_outcomes(self, three_figures: List[Figure]):
+        figures_orders = {p for p in permutations(three_figures)}
+
+        possibilities = []
+        for order in figures_orders:
+            current_order_possibilities = [(self.current_board_state, [])]
+            for figure in order:
+                t1 = time()
+                current_order_possibilities = [
+                    (
+                        prev_board.add_figure(figure, i, j),
+                        prev_history + [(figure, i, j)]
+                    )
+                    for prev_board, prev_history in current_order_possibilities
+                    for i, j in prev_board.possible_figure_places(figure)
+                ]
+                t2 = time()
+                print(f'{(t2 - t1):.3f} sec')
+            possibilities += current_order_possibilities
+
+        return possibilities
+
 
     def update(self, new_board, upscore):
         pass
@@ -161,4 +192,12 @@ class Game(object):
 
 if __name__ == '__main__':
     g = Game()
-    g.round()
+    nf = g.get_new_figures(3, seed=1)
+    for f in nf:
+        print(f)
+        print()
+    po = g.possible_outcomes(nf)
+    print(len(po))
+    po_1_board = po[5000][0]
+    print(po_1_board)
+
